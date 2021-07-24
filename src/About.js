d9df34b46378a7fe60028ed1bbed9c315cca9c05
Siddharth Singh <simsid66@gmail.com>
2021-07-24 21:53:40 +0530

    Add GitHub link to About

--- src/About.js
+++ src/About.js
@@ -10,9 +10,10 @@ class About extends Window {
                 height: '100%'
             },
             innerHTML: `<div style='text-align: center'><b>Pseudows by shantaram</b></div>
+            <div style='text-align: center'>version 0.6.0</div>
             <div style='text-align: center'>This program is free, open-source software under the MIT License.</div>
             <div style='text-align: center'>Copyright © 2021 Siddharth Singh</div>
-            <div style='text-align: center'>version 0.6.0</div>`,
+            <div style='text-align: center'><a href='https://github.com/shantaram3013/pseudows'><i class='fa fa-github'></i> Source code</a></div>`,
             parent: this.elem
         })
     }
